7385aefeaad708424dcaf2dbae95d4257a0dd261
warmflasha <warmflasha@gmail.com>
2017-10-04 18:26:15 -0500

    graded inclass8

--- inclass8.m
+++ inclass8.m
@@ -1,3 +1,6 @@
+%AW: see comments below. 0.9/1.
+
+
 %Inclass assignment 8
 
 %Using the swalign function
@@ -16,12 +19,17 @@ swalign(seq1, seq2, 'Alphabet', 'nt','GapOpen', 1, 'Showscore',true)
 %the higher one makes gaps prohibitively expensive, such that only straight
 %diagonals are allowed, while the lower one allows for lots of gaps and can
 %thus find a path that's not perfectly straight
+%AW: this is correct but what does it mean in terms of the alignments produced. The former
+% favors mismatches, the later favors gaps in the alignment. -.05. 
+
 
 % E. run swalign with a low value of GapOpen but a high value of ExtendGap,
 % explain the result
 swalign(seq1, seq2, 'Alphabet', 'nt','GapOpen', 1, 'ExtendGap', 30, 'Showscore',true)
 %the penalty for extending is too high and with the gap penalty low, it
 %chooses a path with no/few jumps that is not perfectly straight(gaps)
+
+%AW: not quite - it is willing to open gaps because the penalty is low, but they will be short (1bp). 
 
 % F. run swalign align with the default value of GapOpen and a low value
 % of ExtendGap and explain the result. 
@@ -30,3 +38,4 @@ swalign(seq1, seq2, 'Alphabet', 'nt', 'ExtendGap', 1, 'Showscore',true)
 %much lower and it chooses the path with a lot of extensions to different
 %regions that have few gaps (and are straigt diagonals) to satisfy the
 %higher gap penalty
+%AW: i.e. there will be fewer gaps, but they will be longer. 
